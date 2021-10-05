e50f7cc7431ca63689563ad6a58050d72b4af461
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-05 15:32:03 +0200

    Actividad2 corregida

--- src/actividad2/Actividad2.java
+++ src/actividad2/Actividad2.java
@@ -3,12 +3,20 @@ package actividad2;
 public class Actividad2 {
 
     public static void main(String[] args){
-        Tarea tarea = new Tarea(0,1);
+        Tarea tarea0 = new Tarea(0);
+        Tarea tarea1 = new Tarea(1);
 
-        tarea.start();
+
+        tarea0.start();
+        tarea1.start();
 
         try {
-            tarea.join();
+            tarea0.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            tarea1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -18,23 +26,26 @@ public class Actividad2 {
 
 class Tarea extends Thread{
 
-    int idThread0;
-    int idThread1;
+    //Solo necesito una hebra, el segundo id sobra
+    int idThread;
+    //int idThread1;
 
-    Tarea(int idThread0, int idThread1){
-        this.idThread0 = idThread0;
-        this.idThread1 = idThread1;
+    Tarea(int idThread0){
+        this.idThread = idThread0;
     }
-
+    /*
+    Si me han pasado el ID 0, se muestra Hola
+    si es 1, se muestra Adios
+     */
     public void run(){
-        if (this.idThread0 == 0){
+        if (this.idThread == 0){
             for (int i = 0 ; i < 3; i++){
-                System.out.println("Hola");
+                System.out.println("Hola, soy la hebra: "+idThread);
             }
         }
-        if (this.idThread1 == 1){
+        if (this.idThread == 1){
             for (int i = 0 ; i < 3; i++){
-                System.out.println("Adios");
+                System.out.println("Adios, soy la hebra "+idThread);
             }
         }
     }
