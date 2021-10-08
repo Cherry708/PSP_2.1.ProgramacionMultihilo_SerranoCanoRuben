727b9d8901bb24b5cca14610bc5af352ac50be7c
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-08 17:48:16 +0200

    Practica 2 synchronized y volatile completadas

---
+++ src/practica2synchronized/Practica2Synchronized.java
@@ -0,0 +1,66 @@
+package practica2synchronized;
+
+public class Practica2Synchronized {
+    public static void main (String args[] ) {
+        if ( args.length != 1 ) {
+            System.out.println ("Error");
+            System.exit(-1);
+        }
+
+        int numHebras = Integer.parseInt (args [0] );
+
+        Hebra[] vectorHebras = new Hebra[ numHebras ];
+        CuentaIncrementos contador = new CuentaIncrementos();
+
+        System.out.println ( "Soy el programa principal y contador vale: " + contador.dameContador());
+
+        for ( int hebra = 0; hebra < numHebras; hebra++ ) {
+            vectorHebras[hebra] = new Hebra(hebra, contador);
+        }
+
+        for ( int hebra = 0; hebra < numHebras; hebra++ ) {
+            vectorHebras[hebra].start();
+
+        }
+
+        for (int hebra = 0; hebra < numHebras; hebra++ ) {
+            try{
+                vectorHebras[hebra].join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+
+            }
+        }
+
+        System.out.println ("Soy el programa principal y contador vale: " + contador.dameContador());
+    }
+}
+class Hebra extends Thread{
+    int idHebra;
+    CuentaIncrementos contador;
+    public Hebra(int idHebra, CuentaIncrementos contador){
+        this.idHebra = idHebra;
+        this.contador = contador;
+    }
+    public void run(){
+        System.out.println("Inicia la hebra: "+idHebra);
+        for (int i = 0; i < 1000000; i++){
+            contador.incrementaContador();
+        }
+        System.out.println("La hebra "+idHebra+" ha finalizado el incremento");
+    }
+}
+
+class CuentaIncrementos{
+
+    long contador = 0;
+
+    synchronized void incrementaContador(){
+        contador++;
+    }
+
+    synchronized long dameContador(){
+        return this.contador;
+    }
+
+}
